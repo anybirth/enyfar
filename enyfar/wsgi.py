df84cde73a5ec69bc2f38b212fd6320c78e17091
tab0902 <mercury.brave.0902@gmail.com>
2018-04-07 15:54:31 +0900

    Change project name to 'enyfar' from 'anybirth'

--- enyfar/wsgi.py
+++ enyfar/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for anybirth project.
+WSGI config for enyfar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anybirth.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "enyfar.settings")
 
 application = get_wsgi_application()
